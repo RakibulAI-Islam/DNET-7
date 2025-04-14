616d57a9834a17f3bdbc68e483f3e8a60ca21ded
Rakibul_AI <rakib.ice07@gmail.com>
2025-04-15 02:33:45 +0600

    3.'Simplify' 'HomeController'.

--- C-3/PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ C-3/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -6,13 +6,15 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
+        /*private readonly ILogger<HomeController> _logger;*/
+         public IActionResult Index()
+         {
+            return View();
+         }
+            /*_logger = logger;*/
         }
 
+        /*
         public IActionResult Index()
         {
             return View();
@@ -27,6 +29,6 @@ namespace PartyInvites.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+        }*/
+    
 }
